86691217e22d058a195cd1d4aed90030261a53d5
Seydou Toloba <seydou.toloba.st@gmail.com>
2021-10-10 22:24:02 +0000

    init10

--- djagodeme/settings.py
+++ djagodeme/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, "static"),
+    os.path.join(BASE_DIR, "static"),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
