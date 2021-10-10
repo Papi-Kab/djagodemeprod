9df320af5b4fa4464cfcb872cff9fe63cd60dacb
Seydou Toloba <seydou.toloba.st@gmail.com>
2021-10-10 22:12:56 +0000

    init8

--- djagodeme/settings.py
+++ djagodeme/settings.py
@@ -124,9 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(PROJECT_ROOT, "static"),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -141,4 +142,4 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
